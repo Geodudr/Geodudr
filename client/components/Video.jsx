50bc9c815e8da52077797a96aa1b75617c364afb
Dhruv Thota <dhruv@dhruvs-mbp.lan>
2022-12-07 10:25:18 -0600

    Pushing to prod

--- client/components/Video.jsx
+++ client/components/Video.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 const Video = () => {
 
+  // const scriptOne = document.createElement("script");
+  // scriptOne.src = "https://unpkg.com/peerjs@1.4.7/dist/peerjs.min.js";
+  // scriptOne.async = true;
+
+  // const scriptTwo = document.createElement("script");
+  // scriptTwo.src = "/socket.io/socket.io.js";
+  // scriptTwo.async = true;
+
+  // const scriptThree = document.createElement('script');
+  // scriptThree.src = "script.js";
+  // scriptThree.async = true;
+
   return (
       <video></video>
   );
